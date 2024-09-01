8ff2c34bf007af3a2a4977147419648459a3a1b6
1Storm3 <zemskoff2013@yandex.ru>
2024-09-01 21:15:28 +0300

    start server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Start api kinopoisk")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "Hello, welcome to the Kinopoisk API!")
+	})
+
+	port := "8080"
+	fmt.Printf("Starting server on port %s...\n", port)
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
